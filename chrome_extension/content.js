fd62af3806de0a36bf5c227d66c94cbef73630d9
mhaya <masaharu.hayashi3@gmail.com>
2016-02-13 06:16:05 +0900

    first commit

---
+++ chrome_extension/content.js
@@ -0,0 +1,9 @@
+var img_ele = document.getElementsByTagName("IMG");
+img_ele[0].className = 'annotatable';
+
+/*
+for (var i=0; i<img_ele.length; i++){
+    img_ele[i].className = 'annotatable';
+    console.log(img_ele[i]);
+}
+*/
