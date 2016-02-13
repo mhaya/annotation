0093b97b73ee67723ad682841b7c649f0fd3863b
mhaya <masaharu.hayashi3@gmail.com>
2016-02-14 02:17:00 +0900

    add option page

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,2 +1,21 @@
+var anno_data;
+var getAnnotation = function(){
+    console.log("getAnnotation");
+    console.log(anno_data);
+
+    return anno_data;
+};
+var updateAnnotation = function( data ){
+    anno_data = data;
+    console.log("updateAnnotation");
+    console.log(data);
+};
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    console.log(request);
+  sendResponse({tabid:sender.tab.id});
+});
+
 chrome.browserAction.onClicked.addListener(function(tab) {
 });
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,9 +1,12 @@
-var img_ele = document.getElementsByTagName("IMG");
-img_ele[0].className = 'annotatable';
+$(function(){
+    var img_ele = document.getElementsByTagName("IMG");
+    img_ele[0].className = 'annotatable';
 
-/*
-for (var i=0; i<img_ele.length; i++){
-    img_ele[i].className = 'annotatable';
-    console.log(img_ele[i]);
-}
-*/
+    // ボタンを追加する
+    $("body").append('<input type="button" id="save_btn" value="save" />');
+    $("#save_btn").click( function() {
+	var d = JSON.stringify(anno.getAnnotations());
+	// backgroundにアノテーションを送信する
+	chrome.runtime.sendMessage({annotations: d}, function(response) {console.log(response);});
+    });
+});
