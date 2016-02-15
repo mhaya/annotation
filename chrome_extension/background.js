0dba1541d8ce4817506640368bf693c1b4ce050e
mhaya <masaharu.hayashi3@gmail.com>
2016-02-16 01:32:09 +0900

    update chrome etension

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,21 +1,48 @@
-var anno_data;
-var getAnnotation = function(){
+$(function(){
+    var anno_data;
+
+    var getAnnotation = function(){
     console.log("getAnnotation");
     console.log(anno_data);
 
     return anno_data;
-};
-var updateAnnotation = function( data ){
+
+    };
+
+    var updateAnnotation = function( data ){
     anno_data = data;
     console.log("updateAnnotation");
     console.log(data);
-};
+
+    };
 
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    console.log(request);
-  sendResponse({tabid:sender.tab.id});
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	var annojson = request["annotations"];
+	var uri = request["uri"];
+	var url = localStorage["anno_url"];
+	var width = request["width"];
+	var height = request["height"];
+
+	console.log(request);
+	$.ajax({
+	    type: "POST",
+	    url: url,
+	    xhrFields: {withCredentials: true},
+	    data: {"update":1,"uri": uri, "annojson": annojson, "width": width,"height": height,"edit":1}
+	}).success(function(data){
+	    console.log("success"+data);
+	}).error(function(data){
+	    console.log("error:"+data);
+	});
+
+	sendResponse({tabid:sender.tab.id});
+    });
+
+
+    chrome.browserAction.onClicked.addListener(function(tab) {
+    });
+
 });
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-});
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -5,8 +5,19 @@ $(function(){
     // ボタンを追加する
     $("body").append('<input type="button" id="save_btn" value="save" />');
     $("#save_btn").click( function() {
-	var d = JSON.stringify(anno.getAnnotations());
+	// アノテーションを取得する
+	var a = anno.getAnnotations();
+	// JSON文字列に変換する
+	var d = JSON.stringify(a);
+
+	// 画像の幅，高さを取得する
+	var img = new Image();
+	img.src = a[0].src;
+	var width = img.width;
+	var height = img.height;
+
+	
 	// backgroundにアノテーションを送信する
-	chrome.runtime.sendMessage({annotations: d}, function(response) {console.log(response);});
+	chrome.runtime.sendMessage({"uri": a[0].src, "annotations": d, "width": width, "height": height}, function(response) {console.log(response);});
     });
 });
